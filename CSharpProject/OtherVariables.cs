ab5d2da24059a0ba49ea0e7d8625cc07d246c046
sayginguven <sayginguven@yahoo.com>
2019-04-18 11:40:32 -0700

    string type and some methods

--- CSharpProject/OtherVariables.cs
+++ CSharpProject/OtherVariables.cs
@@ -47,7 +47,36 @@ namespace CSharpProject
             Console.WriteLine("\n");
 
 
-            
+            string testString = "Hellooooo C sharp!   ";
+            Console.WriteLine($"original string is \t{testString}");
+            Console.WriteLine($"testString.Substring(5) \t{testString.Substring(5)}");
+            Console.WriteLine($"testString.Substring(10,7) \t{testString.Substring(10,7)}");
+            Console.WriteLine($"testString.Remove(5) \t{testString.Remove(5)}");
+            Console.WriteLine($"testString.Remove(5,4) \t{testString.Remove(5,4)}");
+            Console.WriteLine(testString.Replace(" sharp!", "#").Trim().ToUpper().Remove(5, 4));
+            Console.WriteLine($"to lower case : {testString.ToLower()}");
+            Console.WriteLine($"the length of testString is : {testString.Length}");
+            Console.WriteLine("\n");
+
+            string e = "my name is ";
+            string m = "what ";
+            string i = "my name is ";
+            string n = "what ";
+            string e1 = "my name is ";
+            string m1 = "slim shaddy";
+
+            Console.WriteLine(e+m+i+n+e1+m1);
+            Console.WriteLine("\n");
+
+            StringBuilder myFavoriteSong = new StringBuilder("LET IT GO!!\n");
+            myFavoriteSong.AppendLine("let it go ");
+            myFavoriteSong.AppendLine("let it go ");
+            myFavoriteSong.AppendLine("can't hold it back anymore ");
+            myFavoriteSong.AppendLine("let it go ");
+            myFavoriteSong.AppendLine("turn away and slam the door.");
+            myFavoriteSong.AppendLine("the perfect girl is gone");
+            Console.WriteLine(myFavoriteSong.ToString());
+
 
 
         }
