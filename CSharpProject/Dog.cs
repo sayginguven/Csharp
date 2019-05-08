295d8c853961993da7fbbc5e1035008b0368d7ff
sayginguven <sayginguven@yahoo.com>
2019-05-08 12:27:25 -0700

    added dog class and 2 interfaces

---
+++ CSharpProject/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    class Dog :  ISpecial
+    {
+
+        public bool IsAlive { get; set; }
+
+        public void Swim() {
+            Console.WriteLine("yeea I like water");
+        }
+
+        public void Move()
+        {
+            Console.WriteLine("I move as a dog");
+        }
+
+        public void Poo()
+        {
+            Console.WriteLine("I poo");
+
+        }
+
+    }
+}
---
+++ CSharpProject/Icreatures.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProject
+{
+    interface ICreatures
+    {
+        bool IsAlive { get; set; }
+        void Move();
+        void Poo();
+    }
+}
